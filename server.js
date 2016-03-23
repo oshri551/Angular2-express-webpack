da7ac750acd6c0c5880f92786ffc6237d93ba187
oshri551 <oshri551@gmail.com>
2016-03-24 01:16:53 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config.js');
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 3500 : process.env.PORT;
+const app = express();
+const router = express.Router();
+
+if(isDeveloping) {
+    config.metadata.express = true;
+    const compiler = webpack(config);
+    const middleware = webpackMiddleware(compiler, {
+        publicPath: config.output.publicPath,
+        contentBase: 'src',
+        stats: {
+            colors: true,
+            hash: false,
+            timings: true,
+            chunks: false,
+            chunkModules: false,
+            modules: false
+        }
+        
+    })
+    app.use('/api', router);
+    
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler));
+    router.get('/', function response(req, res) {
+        res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+        res.end();
+    });
+} else {
+  app.use(express.static(__dirname + '/dist'));
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
+
+
+
+router.get('/test', function(req, res){
+    res.json({ messege: 'Express rest API is workin !!'});
+})
+
+
+
+app.listen(port, '0.0.0.0', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
+
